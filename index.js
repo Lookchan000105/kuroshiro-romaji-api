1bee3d0a7d65b5f7d6b9586e827e32ecb692638d
Lookchan000105 <150579689+Lookchan000105@users.noreply.github.com>
2025-05-04 08:06:29 +0800

    fix Kuroshiro import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const Kuroshiro = require("kuroshiro");
+const Kuroshiro = require("kuroshiro").default;
 const KuromojiAnalyzer = require("kuroshiro-analyzer-kuromoji");
 
 const app = express();
